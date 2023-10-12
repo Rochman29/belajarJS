fcbe702c95c66f4fceb24e8ec22b59f04a3ec89a
Rochman29 <rochmanbambanges29@upi.edu>
2023-10-12 17:08:45 +0700

    second commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// console.log("Hello World");
+// alert("Ada Notifikasi");
+// prompt("Pinjam Seratus");
+
+// var Promnet = "Coding is Easy";
+// console.log(Promnet);
+// var Promnet = "Coding is not Easy";
+// console.log(Promnet);
+// var Promnet = "nothing is possible";
+// console.log(Promnet);
+
+// let Promnet = "Coding is Fun";
+// console.log(Promnet);
+// let Promnet = "Coding is not Fun";
+// console.log(Promnet);
+
+// const Promnet = "Coding is My Life";
+// console.log(Promnet);
+// const Promnet = "Coding is not My Life";
+// console.log(Promnet);
+
+// let totalPoint = prompt("Masukkan Poin Anda: ");
+// if (totalPoint >= 100) {
+//   document.write("Congratulation");
+// } else {
+//   document.write("Keep Trying");
+// }
+// let x = 6;
+// let y = 3;
+
+// console.log(x < 10 && y > 1);
+// console.log(x < 10 && y < 1);
+// console.log(x == 5 || y == 5);
+// console.log(x == 6 || y == 5);
+// console.log(!(x == y));
+
+// document.write(x < 10 && y > 1);
+// document.write("<br>");
+// document.write(x < 10 && y < 1);
+// document.write("<br>");
+// document.write(x == 5 || y == 5);
+// document.write("<br>");
+// document.write(x == 6 || y == 5);
+// document.write("<br>");
+// document.write(!(x == y));
+// let p = document.querySelector("p");
+// let button = document.querySelector("button");
+// let input = document.querySelector("input");
+
+// button.addEventListener("click", function () {
+//   let isi = input.value;
+//   console.log(isi);
+//   p.innerHTML = isi;
+// });
